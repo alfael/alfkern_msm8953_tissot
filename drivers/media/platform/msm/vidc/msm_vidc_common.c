9d7009f09b5b3d9e3542e678d7c8b6639103845c
David Lin <dtwlin@google.com>
2018-03-30 17:09:59 -0700

    msm: vidc: fix issue with unbalanced mutex
    msm_vidc_common.c:1797 handle_event_change() warn: inconsistent returns
    'mutex:&inst->lock'
    
    Bug: 77543262
    Change-Id: Ibdd63f31c7264fa7b7004ac0aa49537a54450f3a
    Signed-off-by: David Lin <dtwlin@google.com>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- drivers/media/platform/msm/vidc/msm_vidc_common.c
+++ drivers/media/platform/msm/vidc/msm_vidc_common.c
@@ -1783,6 +1783,7 @@ static void handle_event_change(enum hal_command_response cmd, void *data)
 			dprintk(VIDC_ERR,
 				"Failed : No buffer requirements : %x\n",
 					HAL_BUFFER_OUTPUT);
+			mutex_unlock(&inst->lock);
 			return;
 		}
 
@@ -1794,6 +1795,7 @@ static void handle_event_change(enum hal_command_response cmd, void *data)
 			dprintk(VIDC_ERR,
 				"Failed : No buffer requirements : %x\n",
 					HAL_BUFFER_OUTPUT2);
+			mutex_unlock(&inst->lock);
 			return;
 		}
 
@@ -1806,6 +1808,7 @@ static void handle_event_change(enum hal_command_response cmd, void *data)
 			dprintk(VIDC_ERR,
 				"Failed : No buffer requirements : %x\n",
 					HAL_BUFFER_OUTPUT);
+			mutex_unlock(&inst->lock);
 			return;
 		}
 		bufreq->buffer_count_min = event_notify->capture_buf_count;
