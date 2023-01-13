67e03ac0e1ecc0803996ff151109e5253cc3805e
alfael <alfael.sc@gmail.com>
2023-01-13 11:59:08 +0100

    package: Update ramdisk file

--- package/anykernel.sh
+++ package/anykernel.sh
@@ -15,23 +15,31 @@ supported.versions=10, 10.0, 11, 11.0, 12, 12.0
 supported.patchlevels=2019-06 -
 '; } # end properties
 
+### AnyKernel install
+# begin attributes
+attributes() {
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+} # end attributes
+
 # shell variables
 block=/dev/block/platform/soc/7824900.sdhci/by-name/boot;
 is_slot_device=1;
 ramdisk_compression=auto;
 
-
-## AnyKernel methods (DO NOT CHANGE)
-# import patching functions/variables - see for reference
-. tools/ak3-core.sh;
-
-
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh && attributes
 
 ## AnyKernel install
 dump_boot;
+
+insert_line init.recovery.qcom.rc "import /init.alfkern.rc" after "import /init.recovery.qcom.usb.rc" "import /init.alfkern.rc";
+
+# fstab.tuna
+backup_file fstab.tuna;
+patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
+patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
+patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
+append_file fstab.tuna "usbdisk" fstab;
 
 write_boot;
