17c1adfa823c8f9be1c807856aa7905bb5f5ce3a
alfael <alfael.sc@gmail.com>
2023-01-30 17:22:09 +0100

    cook_neutron: Rework

--- cook_neutron.sh
+++ cook_neutron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd $(dirname $0)
 export PATH="$HOME/toolchain/neutron-clang/bin:$PATH"
-#export LD_LIBRARY_PATH="$HOME/toolchain/vortex-clang/lib"
+# export LD_LIBRARY_PATH="$HOME/toolchain/neutron-clang/lib"
 export KBUILD_BUILD_USER=alfael
 export KBUILD_BUILD_HOST=LI-DEV
 export CROSS_COMPILE=aarch64-linux-gnu-
@@ -9,7 +9,7 @@ export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_CFLAGS="-Wno-maybe-uninitialized -Wno-memset-elt-size -Wno-duplicate-decl-specifier"
-DEFCONFIG_FILE=tissot_defconfig
+DEFCONFIG_FILE=alfkern_defconfig
 if [ ! -d "./output/" ]; then
         mkdir ./output/
 fi
@@ -17,11 +17,11 @@ fi
 make CC=clang O=output clean
 make CC=clang O=output mrproper
 make CC=clang O=output $DEFCONFIG_FILE
-make CC=clang O=output CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip  -j4 2>&1 | tee build.log
+make CC=clang O=output LD=ld.lld CC=clang AS=llvm-as AR=llvm-ar NM=llvm-nm OBJSIZE=llvm-size READELF=llvm-readelf OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LLVM=1 LLVM_IAS=1 -j4 2>&1 | tee build.log
 
 #Generation package anykernel
 PATH_OUTPUT=output/arch/arm64/boot
-PATH_KERN=$PATH_OUTPUT/Image.gz
+PATH_KERN=$PATH_OUTPUT/Image.gz-dtb
 PATH_QCOM=$PATH_OUTPUT/dts/qcom/
 PATH_PACKAGE=package
 PATH_OUTPUT_PACKAGE=~/kernel/output
@@ -55,9 +55,9 @@ done < arch/arm64/configs/$DEFCONFIG_FILE
 
 /bin/cp -rf $PATH_KERN $PATH_PACKAGE
 cd $PATH_PACKAGE
-if [ -f $PATH_OUTPUT_PACKAGE/$LOCALVERSION.$SUBLEVEL.vortex.zip ]; then
-  rm $PATH_OUTPUT_PACKAGE/$LOCALVERSION.$SUBLEVEL.vortex.zip
+if [ -f $PATH_OUTPUT_PACKAGE/$LOCALVERSION.$SUBLEVEL.neutron.zip ]; then
+  rm $PATH_OUTPUT_PACKAGE/$LOCALVERSION.$SUBLEVEL.neutron.zip
 fi
-zip -9 -r $PATH_OUTPUT_PACKAGE/$LOCALVERSION.$SUBLEVEL.vortex.zip ./*
+zip -9 -r $PATH_OUTPUT_PACKAGE/$LOCALVERSION.$SUBLEVEL.neutron.zip ./*
 cd -;
-echo Création du package: $LOCALVERSION.$SUBLEVEL.vortex.zip terminée !
+echo Création du package: $LOCALVERSION.$SUBLEVEL.neutron.zip terminée !
